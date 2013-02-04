9de3f83d8b2860d91b1e761507e800cf1d42b293
Daniel Hall <daniel.hall@rea-group.com>
2013-02-04 18:21:05 +1100

    This is how its meant to be done

--- Lager/urls.py
+++ Lager/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.conf import settings
 
 from django.contrib import admin
@@ -13,9 +14,4 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    # Media files
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-
-)
+)  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
