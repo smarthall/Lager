f8d9a15872dfa89526726e6b7961b4c27c7bfe9a
Daniel Hall <daniel@danielhall.me>
2013-02-06 09:14:40 +1100

    Hide the Site model from the admin page

--- Lager/urls.py
+++ Lager/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from django.contrib import admin
+from django.contrib.sites.models import Site
+
 admin.autodiscover()
+admin.site.unregister(Site)
 
 urlpatterns = patterns('',
     # Examples:
